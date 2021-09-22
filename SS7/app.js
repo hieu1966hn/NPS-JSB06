f62258fd7104c484dccc0451e687e83987f00e4f
hieu1966hn <hieu1966hn@gmail.com>
2021-09-22 19:49:40 +0700

    done C,R,U,D with Object

--- SS7/app.js
+++ SS7/app.js
@@ -26,4 +26,27 @@ let Hieu = {
 console.log(Hieu);
 // =>  Thuộc tính làm nên đối tượng đó
 
-//  thêm 1 thuộc tính weight vào đối tượng trên
+
+/// Thao tác C,R,U,D đối với Object
+
+
+
+
+// C:  thêm 1 thuộc tính weight vào đối tượng trên
+Hieu.weight = "70kg" // Nếu như đã có thuộc tính này, => update giá trị => 70
+// Nếu như chưa có thuộc tính này=> thêm mới vào Object.
+
+
+console.log("Đối tượng Hieu sau khi them thuộc tính weight là:", Hieu);
+
+
+// R: Sử dụng console.log(obj)
+
+// U: Update 1 thuộc tính trong Object
+Hieu.weight = "65kg";
+console.log("Đối tượng Hieu sau khi update thuộc tính weight là:", Hieu);
+
+
+// D: Delete 1 thuộc tính trong Object: 
+delete Hieu.weight;
+console.log("Đối tượng Hieu sau khi Delete thuộc tính weight là:", Hieu);
